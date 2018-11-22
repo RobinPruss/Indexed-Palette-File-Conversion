c3fa9c02eb7a63e3eaae2a45bc88dc223fd582eb
RobinPruss <code.writer@robinpruss.com>
2018-11-22 16:28:38 -0500

    birdClass added for global use

--- toolIndexedPaletteFileConversion.py
+++ toolIndexedPaletteFileConversion.py
@@ -1,8 +1,23 @@
 #toolIndexedPaletteFileConversion.py
 #(C) 2018 Robin Pruss
 
-def main():
+import math
+import re
+
+
+class birdClass():
     pass
+
+
+def setupRegex(bird):
+    bird.cdfInput = re.compile(r'(.{34})(.{11})(.{11})(.{9})', flags=re.I)
+
+
+def main():
+    bird = birdClass()
+    setupRegex(bird)
+    print('done')
+
 
 if __name__ == "__main__":
     main()
