c080bef1e16f9a6b1bc5a3a415260437ad059fba
RobinPruss <code.writer@robinpruss.com>
2018-11-27 14:54:42 -0500

    choice of .cdf files in a folder

--- toolIndexedPaletteFileConversion.py
+++ toolIndexedPaletteFileConversion.py
@@ -2,32 +2,57 @@
 #(C) 2018 Robin Pruss
 
 import io
+import os
 import re
 
 
 class birdClass():
-    pass
+    fromFile = None
+    toFile = None
 
 
 def setupRegex(bird):
+    bird.cdfExt = re.compile(r''' 
+        (?P<eFN>.*)             #any number of characters
+        \.                      #escape for "."
+        cdf                     #extension for cdf
+        $                       #at end of line
+        ''', re.I | re.X)
     bird.cdfInput = re.compile(r'''
-        (?P<rawDesc>.{34})      #1 description string
-        (?P<rawR>.{11})         #2 R value 0.0-1.0
-        (?P<rawG>.{11})         #3 G value 0.0-1.0
-        (?P<rawB>.{9})          #4 B value 0.0-1.0
+        (?P<rawDesc>.{34})      #description string
+        (?P<rawR>.{11})         #R value 0.0-1.0
+        (?P<rawG>.{11})         #G value 0.0-1.0
+        (?P<rawB>.{9})          #B value 0.0-1.0
         ''', re.I | re.X)
     bird.gplInput = re.compile(r'''
-        (?P<rawR>\d+)\s+        #1 R value 0-255
-        (?P<rawG>\d+)\s+        #2 G value 0-255
-        (?P<rawB>\d+)\s+        #3 B value 0-255
-        (?P<rawDesc>.*)         #4 description string
+        (?P<rawR>\d+)\s+        #R value 0-255
+        (?P<rawG>\d+)\s+        #G value 0-255
+        (?P<rawB>\d+)\s+        #B value 0-255
+        (?P<rawDesc>.*)         #description string
         ''', re.I | re.X)
 
 
 def chooseFiles(bird):
-    bird.fromFileName = r'C:\TEMP\ugcolor.cdf'
-    bird.toFileName = r'C:\TEMP\ugcolor_01.gpl'
-    pass
+    
+    foundFiles = []
+    while foundFiles == []:
+        tempPathName = input('Enter a folder path with .cdf files >> ')
+        with os.scandir(tempPathName) as thisPath:
+            for thisFile in thisPath:
+                result = bird.cdfExt.search(thisFile.name)
+                if result:
+                    foundFiles.append(result.group('eFN'))
+    if len(foundFiles) == 1:
+        x = 0
+    else:
+        foundFiles.sort
+        for y, z in enumerate(foundFiles):
+            print(str(y), ' ', z)
+        x = int(input('Which of these files? >> '))
+    bird.fromFileName = os.path.join(tempPathName, 
+                                     foundFiles[x] + '.cdf')
+    bird.toFileName = os.path.join(tempPathName, 
+                                   foundFiles[x] + '.gpl')
 
 
 def prepareFiles(bird):
@@ -47,13 +72,20 @@ def convertColor255to1(color255):
 
 
 def main():
-    bird = birdClass()
-    setupRegex(bird)
-    chooseFiles(bird)
-    prepareFiles(bird)
-    bird.fromFile.close()
-    bird.toFile.close()
-    print('done')
+    print('''Welcome to toolIndexedPaletteFileConversion.py
+        Copyright 2018, Robin Pruss
+        Licensed under the BSD 3-Clause License
+        This program currently converts from .cdf to .gpl.\n''')
+    try:
+        bird = birdClass()
+        setupRegex(bird)
+        chooseFiles(bird)
+        prepareFiles(bird)
+        bird.fromFile.close()
+        bird.toFile.close()
+        print('done')
+    except:
+        print('ERROR')
 
 
 if __name__ == "__main__":
